bb983f33efef14cc2b821169d70e529e4d65b2b5
girish-marwah <64033268+girish-marwah@users.noreply.github.com>
2020-06-01 00:40:20 +0530

    Update Product.sql

--- SqlServerDatabase/Product.sql
+++ SqlServerDatabase/Product.sql
@@ -3,5 +3,5 @@ CREATE TABLE [dbo].[Product]
 	[Id] INT NOT NULL PRIMARY KEY, 
     [Name] NVARCHAR(50) NULL, 
     [Cost] INT NULL, 
-    [Color2] NVARCHAR(50) NULL
+    [Color] NVARCHAR(50) NULL
 )
